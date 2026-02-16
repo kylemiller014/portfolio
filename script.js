ef3c06935e2e5e5565bfa2120a55086bb1cbd722
Kyle Miller <kylemiller014@gmail.com>
2026-02-16 12:15:23 -0600

    Adjusted mediaqueries.css for updated naming conventions, removed js email, added web3 email service

--- script.js
+++ script.js
@@ -4,22 +4,3 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-
-function sendMail() {
-    var params = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        message : document.getElementById("message").value
-    }
-
-    emailjs
-    .send("service_20pigze", "template_jm79tde", params)
-    .then((res) => {
-       document.getElementById("name").value = "";
-       document.getElementById("email").value = "";
-       document.getElementById("message").value = "";
-       console.log(res);
-       alert("Message sent successfully!");
-    })
-    .catch((err) => console.log(err));
-}
